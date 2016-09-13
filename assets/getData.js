13a900f5a6e5d786408d6bffa87c98daafd31664
john-m-kelly <john_m_kelly@apple.com>
2016-09-13 00:28:33 -0700

    1/64th data, includes forecasts; labels

--- assets/getData.js
+++ assets/getData.js
@@ -26,80 +26,22 @@ var colorGradient = d3.scaleSequential(d3.interpolateViridis);
 colorGradient.domain([0, 1]);
 
 
-d3.json("assets/sfba_land_pts.geojson", function(err, data){
+//d3.json("assets/sfba_land_pts_64.geojson", function(err, data){
+d3.json("assets/sfba_land_pts_64_forecast_data.geojson", function(err, data){
   A.gjPoints = data;
-  //processData2(A.gjPoints);
+  //processData(A.gjPoints);
+  // document.querySelector('body').innerHTML = JSON.stringify(A.gjPoints);
+
   addPoints();
 });
 
 
 
-function processData2() {
-/*  var layersStyler = {
-    version: 8,
-    name: 'Basic',
-    sources: {
-      'us-counties': {
-        'type': 'vector',
-        'url': 'mapbox://devseed.us-counties'
-      }
-    },
-    //sprite: '',
-    //glyphs: '',
-    layers: [{
-      id: 'background',
-      type: 'background',
-      paint: { 'background-color': '#000' }
-    }]
-  };
-*/
-  // the logarithmic scale we'll use
-  var breaks = [0, 4, 16, 64, 256, 1024, 4096, 16384, 65536]
-
-  // for each level, we set the filter to choose features with population
-  // density values between two consecutive values from the scale
-  for (var p = 0; p < breaks.length; p++) {
-    var filters
-    if (p < breaks.length - 1) {
-      filters = [ 'all',
-        [ '>=', 'pop_density', breaks[p] ],
-        [ '<', 'pop_density', breaks[p + 1] ]
-      ]
-    } else {
-      filters = [ 'all',
-        [ '>=', 'pop_density', breaks[p] ]
-      ]
-    }
-
-    //layersStyler.layers.push({
-    map.addLayer({
-      id: 'counties-pop-' + p,
-      type: 'fill',
-      source: 'us-counties',
-      paint: {
-        'fill-color': '#5b6b6b',
-        // set the opacity based on the level
-        'fill-opacity': (p + 1) / breaks.length
-      },
-      filter: filters
-    })
-  }
-
-/*
-  var choro = new mapboxgl.Map({
-    container: 'map',
-    style: layersStyler,
-    center: [-74.50, 40],
-    zoom: 3
-  })
-*/
-
-}
 
 function processData(gj) {
 
   gj.features.forEach(function(v,i){
-/*
+
     //console.log(v.properties.name);
     console.log(v.geometry.coordinates);
     var latLonTime = [v.geometry.coordinates[1], v.geometry.coordinates[0], A.nowTime];
@@ -108,26 +50,19 @@ function processData(gj) {
     d3.json(url, function(err, data){
       console.log(err);
       console.log(data.currently.cloudCover);
-      gj.features[i].properties.data = data.currently.cloudCover;
+      //gj.features[i].properties.data = data.currently.cloudCover;
+      gj.features[i].properties.data = data;
+
+      sun = 1 - data.currently.cloudCover;
+      sun = Math.round(sun * 100) / 100
+      gj.features[i].properties.sun = sun;
+      //gj.features[i].properties["color"] = colorGradient(sun);
     });
-*/
-    sun = 1 - gj.features[i].properties.data;
-    sun = Math.round(sun * 100) / 100
-    gj.features[i].properties.data = sun;
-    gj.features[i].properties["color"] = colorGradient(sun);
+
   });
 
 }
 
-
-
-/*
-    map.addSource("us-counties", {
-        'type': 'vector',
-        'url': 'mapbox://devseed.us-counties'
-    })*/
-
-    //processData2();
 
 function addPoints() {
 
@@ -143,22 +78,29 @@ function addPoints() {
         "source": "points",
         "paint": {
             "circle-color": {
-              property: 'data',
+              property: 'sun',
               stops: [
-                  [0, '#ffff00'], // sfba_land_pts.geojson = cloudiness - 1=overcast, 0=sunny
-                  [0.55, '#0033ff'] // max cloudy for that datetime
+                  [0, '#0033ff'], // sfba_land_pts.geojson = cloudiness - 1=overcast, 0=sunny
+                  [0.55, '#ff7700'] // max cloudy for that datetime
               ]
             },
-            "circle-radius": 14,
+            "circle-radius": 9,
             "circle-opacity": 0.5
-        }/*,
+        }
+    });
+
+    map.addLayer({
+        "id": "points3",
+        "type": "symbol",
+        "source": "points",
         "layout": {
             "icon-image": "{icon}-15",
-            "text-field": "{data}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
+            "text-field": "{sun}",
+            "text-size": 8,
+            //"text-font": ["Open Sans Regular", "Arial Unicode MS Regular"],
+            "text-offset": [0, -0.5],
             "text-anchor": "top"
-        }*/
+        }
     });
 
   });
@@ -175,12 +117,7 @@ function addPoints() {
       var feature = features[0];
 
       var data = feature.properties.name + ": " + feature.properties.data || "no data";
-      //if (feature.properties.data.currently) {}
-      /*console.log(data.currently);
-      if (feature.properties.data.currently.cloudCover) {
-        data = feature.properties.data.currently.cloudCover;
-      }
-  */
+
       // Populate the popup and set its coordinates
       // based on the feature found.
       var popup = new mapboxgl.Popup()
