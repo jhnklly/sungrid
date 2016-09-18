2f23418bacd3fda8cf2089540c5a94fc494ed9f6
john-m-kelly <john_m_kelly@apple.com>
2016-09-17 17:55:26 -0700

    Switch to mapsense / d3.

--- assets/getData.js
+++ assets/getData.js
@@ -1,5 +1,78 @@
-// getData.js
-//https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE,TIME
+/*
+getData.js
+https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE,TIME
+
+When storing fio data, hash to minimize?
+No, keep full copies, and create jsons with only desired keys,
+renamed to abbr.
+
+Gather data from all requests into one large json.
+Write to file/db for that date, showing number of requests and NE corner.
+
+Use large json to animate? Or use hourly info, already stored on each pt.
+Play/pause
+Global animTime counter.
+
+d3 instead? Better transitions; data bound to svg elements.
+
+function cullDatum(obj) {
+  var newDatum = {
+    "t": obj.time,
+    "pp": obj.precipProbability,
+    "tempF": obj.temperature,
+    "w": obj.windSpeed,
+    "vis": obj.visibility,
+    "cc": obj.cloudCover,
+    "prsr": obj.pressure
+  };
+  return newDatum;
+}
+
+
+function cullDatumDaily(obj) {
+  var newDatum = {
+    "t": obj.time,
+    "pp": obj.precipProbability,
+    "tempFMax": obj.temperatureMax,
+    "tempFMin": obj.temperatureMin,
+    "w": obj.windSpeed,
+    "vis": obj.visibility,
+    "cc": obj.cloudCover,
+    "prsr": obj.pressure,
+    sunrise: obj.sunriseTime,
+    sunset: obj.sunrsetTime,
+  };
+  return newDatum;
+}
+
+
+var newFeature = {};
+var raw = a.properties.data;
+
+newFeature.lat = raw.latitude;
+newFeature.lon = raw.longitude;
+newFeature.curr = {
+  "t": raw.currently.time,
+  "pp": raw.currently.precipProbability,
+  "tempF": raw.currently.temperature,
+  "w": raw.currently.windSpeed,
+  "vis": raw.currently.visibility,
+  "cc": raw.currently.cloudCover,
+  "prsr": raw.currently.pressure
+};
+
+newFeature.hourly = { "data": [] };
+newFeature.daily = { "data": [] };
+
+raw.daily.data.forEach(function(v,i){
+  newFeature.daily.data.push(cullDatum(v));
+});
+
+Step/animate:
+Increment daylight hours
+*/
+
+
 var APIKEY = "86f515c3f0103714bc87cfc7910bcdc5";
 var A = {};
 A.gjFeature =  {
@@ -21,6 +94,21 @@ A.GJ = {
 
 //A.gjPoints = returnPointGrid('-122.5 37.5 -122.0 38.0', 1/32); // SFBA 289 pts
 //A.gjPoints = returnPointGrid('-122.75 37.5 -122.0 38.0', 1/64); // SFBA 289 pts
+
+A.IDX = 0;
+function animate() {
+  var si = window.setInterval(function() {
+    // for each feature, update the mapped value
+    A.gjPoints.features.forEach(function(v,i){
+      v.properties.sun = v.properties.data.hourly.data[A.IDX].cloudCover;
+      console.log(A.IDX, v.properties.sun);
+    });
+    map.getSource('points').setData(A.gjPoints);
+
+    A.IDX++;
+    A.IDX = (A.IDX >= A.gjPoints.features[0].properties.data.hourly.data.length - 1) ? 0 : A.IDX + 1;
+  }, 1000);
+}
 
 var colorGradient = d3.scaleSequential(d3.interpolateViridis);
 colorGradient.domain([0, 1]);
@@ -64,6 +152,40 @@ function processData(gj) {
 }
 
 
+var primary_plus_colors = {
+  //darkRed: '#B21F29',
+  red2: '#ff0000',
+  redOrange: '#ef4623',
+  orange2: '#FF7F00',
+  yellowOrange: '#fdb51e',
+  yellow2: '#ffff00',
+  yellowGreen: '#9fc33e',
+  green2: '#008000',
+  blueGreen: '#157D83',
+  azure2: '#007FFF',
+  //bluePurple: '#f83f99',
+  not_purple2: '#7F00FF',
+  purple2: '#800080'
+};
+
+
+var keysReverse = Object.keys(primary_plus_colors).reverse();
+
+var stops = [[0.0, "#000000"]];
+var j = 0.10;
+
+for(i=0; i< keysReverse.length; i++) {
+    console.log(primary_plus_colors[keysReverse[i]]);
+  //j += 0.02;
+  j += 0.08;
+  stops.push( [j, primary_plus_colors[ keysReverse[i] ] ] );
+}
+/*
+for (var key in primary_plus_colors) {
+  j += 0.02;
+  stops.push([j, primary_plus_colors[key]]);
+}
+*/
 function addPoints() {
 
   map.on('load', function () {
@@ -72,6 +194,7 @@ function addPoints() {
         "data": A.gjPoints
     })
 
+
     map.addLayer({
         "id": "points2",
         "type": "circle",
@@ -79,13 +202,21 @@ function addPoints() {
         "paint": {
             "circle-color": {
               property: 'sun',
-              stops: [
-                  [0, '#0033ff'], // sfba_land_pts.geojson = cloudiness - 1=overcast, 0=sunny
-                  [0.55, '#ff7700'] // max cloudy for that datetime
-              ]
+              stops: stops // [
+                  //[0.10, '#0033ff'], // nb whether value is cloudiness or sunniness
+                  //[0.55, '#ff7700'] //
+                  /*[0.0, '#000000'],
+                  [0.10, '#7F00FF'],
+                  [0.12, '#0000ff'],
+                  [0.14, '#008000'],
+                  [0.16, '#ffff00'],
+                  [0.18, '#ff7f00'],
+                  [0.2, '#ff0000'],
+                  [0.22, '#ff00ff']*/
+              //]
             },
             "circle-radius": 9,
-            "circle-opacity": 0.5
+            "circle-opacity": 0.25
         }
     });
 
@@ -96,8 +227,8 @@ function addPoints() {
         "layout": {
             "icon-image": "{icon}-15",
             "text-field": "{sun}",
-            "text-size": 8,
-            //"text-font": ["Open Sans Regular", "Arial Unicode MS Regular"],
+            "text-size": 12,
+            "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
             "text-offset": [0, -0.5],
             "text-anchor": "top"
         }
